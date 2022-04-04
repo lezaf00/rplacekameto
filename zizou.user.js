6433cf8f912d6e54efa27b324af3d878f61cad54
lezaf00 <102932436+lezaf00@users.noreply.github.com>
2022-04-04 03:24:24 +0200

    Update zizou.user.js

--- zizou.user.js
+++ zizou.user.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  ZIZOU
 // @author       LEZAF
+// @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
 // ==/UserScript==
