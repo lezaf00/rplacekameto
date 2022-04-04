87bf40c3a2efa66fd65c886ba37a6b00efe001cf
lezaf00 <102932436+lezaf00@users.noreply.github.com>
2022-04-04 13:22:34 +0200

    Update kametov2.user.js

--- kametov2.user.js
+++ kametov2.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
 window.addEventListener('load', () => {
     // Load the image
     const image = document.createElement("img");
-    image.src = "https://github.com/lezaf00/rplacekameto/raw/main/reddit-kameto-update_v2.png";
+    image.src = "https://github.com/lezaf00/rplacekameto/blob/main/overlay.png";
     image.onload = () => {
         image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
     };
