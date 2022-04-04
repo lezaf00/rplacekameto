bb0e3ef59bf910055fcf3b258c29df5d6f541a0c
lezaf00 <102932436+lezaf00@users.noreply.github.com>
2022-04-04 02:57:20 +0200

    Update zizou.js

--- zizou.js
+++ zizou.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.3
 // @description  try to take over the canvas!
-// @author       oralekin, LittleEndu, ekgame
+// @author       lezaf
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
