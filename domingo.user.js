b85a1f605dfcb61538f4d9509ea7e8ff1b826f6e
lezaf00 <102932436+lezaf00@users.noreply.github.com>
2022-04-04 20:51:41 +0200

    Update domingo.user.js

--- domingo.user.js
+++ domingo.user.js
@@ -12,7 +12,7 @@ if (window.top !== window.self) {
 window.addEventListener('load', () => {
     // Load the image
     const image = document.createElement("img");
-    image.src = "https://github.com/lezaf00/rplacekameto/blob/raw/popplace.png";
+    image.src = "https://github.com/lezaf00/rplacekameto/raw/main/popplace.png";
     image.onload = () => {
         image.style = `position: absolute; left: 0; top: 0; width: ${image.width/3}px; height: ${image.height/3}px; image-rendering: pixelated; z-index: 1`;
     };
